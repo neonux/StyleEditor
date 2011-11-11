f1beea7ae7556a5006b0c05f79130f24850d4538
Cedric Vivier <cedricv@neonux.com>
2011-11-11 15:20:33 +0800

    Disable all UI animations when testing. Just be conservative.

--- test/ui/head.js
+++ test/ui/head.js
@@ -25,9 +25,11 @@ function launchStyleEditorChrome(aCallback)
   if (gChromeWindow.document.readyState != "complete") {
     gChromeWindow.addEventListener("load", function onChromeLoad() {
       gChromeWindow.removeEventListener("load", onChromeLoad, true);
+      gChromeWindow.styleEditorChrome._alwaysDisableAnimations = true;
       aCallback(gChromeWindow.styleEditorChrome);
     }, true);
   } else {
+    gChromeWindow.styleEditorChrome._alwaysDisableAnimations = true;
     aCallback(gChromeWindow.styleEditorChrome);
   }
 }
